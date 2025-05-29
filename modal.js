f954aca4526f4f8610d90187873462eae78bb3fb
triloon <dimontopcik59@gmail.com>
2025-05-29 21:32:14 +0300

    burger menu

--- modal.js
+++ modal.js
@@ -13,3 +13,32 @@
     document.body.classList.toggle("no-scroll");
   }
 })();
+
+
+
+const hamburgerBtn = document.getElementById('hamburgerBtn');
+        const menuOverlay = document.getElementById('menuOverlay');
+        const mainPage = document.getElementById('mainPage');
+
+        function openMenu() {
+            hamburgerBtn.classList.add('active');
+            menuOverlay.classList.add('active');
+            
+            mainPage.style.filter = 'blur(2px)';
+        }
+
+        function closeMenu() {
+            hamburgerBtn.classList.remove('active');
+            menuOverlay.classList.remove('active');
+            
+            mainPage.style.filter = 'none';
+        }
+
+    
+        document.addEventListener('click', (e) => {
+            if (menuOverlay.classList.contains('active') && 
+                !e.target.closest('.menu-overlay') && 
+                !e.target.closest('.hamburger-btn')) {
+                closeMenu();
+            }
+        });
